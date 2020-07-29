69855fdbd4bc2c2edc0010510ee01eb7711adb3c
Simon Plakolb <s.plakolb@gmail.com>
2020-07-29 22:50:07 +0200

    Add notebook for examples file

---
+++ notebooks/example/Example.hs
@@ -0,0 +1,37 @@
+module Example where
+
+import Foreign.C.Types (CInt)
+import Foreign.C.String
+
+someConstant :: Int
+someConstant = 63
+
+hello :: IO ()
+hello = putStrLn "Hello from Haskell!"
+
+square :: CInt -> CInt
+square i = i * i
+
+multisin :: Int -> Double -> Double
+multisin x y = (fromIntegral x) * (sin y)
+
+pythasLen :: [Integer] -> Int
+pythasLen = length
+
+someComplicatedFunc :: [a] -> IO a
+someComplicatedFunc = undefined
+
+mapQuarter :: [Integer] -> [Double]
+mapQuarter = map ((*0.25) . fromIntegral)
+
+haskellList :: [Int]
+haskellList = [63]
+
+strings :: String -> String
+strings = filter (/= 'a')
+
+nested :: [[String]] -> [[String]]
+nested = id
+
+tuple :: (CInt, String)
+tuple = (1 :: CInt, "a")
